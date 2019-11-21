a3ad514d2a9ff2b942b3e678afb9c8f0fa5d07e0
maulikpipaliya <maulik.pipaliya@gmail.com>
2019-11-21 08:20:54 +0530

    #BugFix - New Added element made draggable

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
 
     });
 
-    dragDrop();
+    // dragDrop();
 
 
     $(document).on("click", ".kk-column-add-task-tb-btn-add", function () {
@@ -53,11 +53,17 @@ $(document).ready(function () {
                 "display": "none"
             })
 
+            var task = $(document).find(".kk-column-task:first");
+            console.log("added task is");
+            
+            console.log(task[0]); //task returns jQ obj, task[0] returns HTML DOM Obj
+            taskAEL(task[0]);
+
             // $(this).parent().prev().remove(); // 
             // $(this).parent().remove();
         }
 
-        dragDrop();
+        // addEventListenersToTasks();
 
 
     });
@@ -70,70 +76,80 @@ $(document).ready(function () {
             "display": "none"
         })
 
-        dragDrop();
+        // addEventListenersToTasks();
         
+
         // $(this).parent().prev().remove();
         // $(this).parent().remove();
     });
     // Drag and drop manipulation - pure JS
-    
+
+    addEventListenersToTasks();
 
 
 }); //Last line of code, document.ready
 
-function dragDrop(){
+function addEventListenersToTasks() {
 
     // var columnAndTasks = getFreshTaskList();
 
     var columnAndTasks = {
-        'columns' : $('.kk-column'),
-        'columnTasks' : $(".kk-column-task")
-    }; 
+        'columns': $('.kk-column'),
+        'columnTasks': $(".kk-column-task")
+    };
     console.log(columnAndTasks);
     columnTasks = columnAndTasks.columnTasks;
     columns = columnAndTasks.columns;
-    
-    
+
     var dragTask = null;
     for (i = 0; i < columnTasks.length; i++) {
         const task = columnTasks[i];
-        
-        task.addEventListener('dragstart', function (e) {
-            console.log("DragStart");
-            dragTask = task;
-            setTimeout(() => {
-                task.style.display = "none";
-            }, 0);
-            // task.style.display = "none";
-        });
-
-        task.addEventListener('dragend', function () {
-            console.log("DragEnd");
-            task.style.display = "block";
-            dragTask = null;
-        });
+        console.log("haha");
+        console.log(task);
+        console.log("lolwa");
+        taskAEL(task);
     }
 
     for (j = 0; j < columns.length; j++) {
         const column = columns[j];
-
-        column.addEventListener('dragover', function (e) {
-            e.preventDefault();
-        });
-
-        column.addEventListener('dragenter', function (e) {
-            e.preventDefault();
-        });
-
-        column.addEventListener('drop', function () {
-            console.log('DropStart');
-            column.append(dragTask);
-        });
+        columnAEL(column);
     }
 
 }
 
 
+function taskAEL(task) {
+    
+    task.addEventListener('dragstart', function (e) {
+        console.log("DragStart");
+        dragTask = task;
+        setTimeout(() => {
+            task.style.display = "none";
+        }, 0);
+        // task.style.display = "none";
+    });
+
+    task.addEventListener('dragend', function () {
+        console.log("DragEnd");
+        task.style.display = "block";
+        dragTask = null;
+    });
+}
+
+function columnAEL(column){
+    column.addEventListener('dragover', function (e) {
+        e.preventDefault();
+    });
+
+    column.addEventListener('dragenter', function (e) {
+        e.preventDefault();
+    });
+
+    column.addEventListener('drop', function () {
+        console.log('DropStart');
+        column.append(dragTask);
+    });
+}
 
 
 var masterObj = {
